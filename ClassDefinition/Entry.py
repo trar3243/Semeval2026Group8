bfa3b23a63315a369fb024b2d589c9c8eea50172
Troy Arthur <trar3243@colorado.edu>
2025-11-14 18:15:48 -0700

    Class Definitions added
    Added ArousalClassifier model. Added Dataset, which stores batch level entries. Batch includes an entry-by-entry roberta CLS extraction. Began work on training loop, including loss. Training loop far from verified; likely to be problematic. Added global environment variable SEMROOT which must be sourced in. Allowed default values for optional arguments. Various tweaks to existing code, particularly Roberta and utils.

--- ClassDefinition/Entry.py
+++ ClassDefinition/Entry.py
@@ -8,8 +8,10 @@ class Entry:
         self.is_words = None
         self.valence = None
         self.arousal = None
+        self.valence_class= None 
+        self.arousal_class = None 
 
     def __repr__(self):
         return (
-            f"Entry(user_id={self.user_id}, text_id={self.text_id}, text={self.text}, timestamp={self.timestamp}, collection_phase={self.collection_phase}, is_words={self.is_words} valence={self.valence}, arousal={self.arousal})"
+            f"Entry(user_id={self.user_id}, text_id={self.text_id}, text={self.text}, timestamp={self.timestamp}, collection_phase={self.collection_phase}, is_words={self.is_words} valence={self.valence}, arousal={self.arousal}, valence_class={self.valence_class}, arousal_class={self.arousal_class})"
         )
--- installDependencies.sh
+++ installDependencies.sh
@@ -1,1 +1,14 @@
-pip install transformers torch ;
+packages=(torch transformers pandas scikit-learn)
+for item in "${packages[@]}"; do
+    if pip show $item &> /dev/null; then
+        echo "Skipping install of $item"
+    else
+        echo "Installing $item..."
+        pip install $item &> /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Completed install of $item"
+        else
+            echo "Failed to install $item"
+        fi 
+    fi
+done 
