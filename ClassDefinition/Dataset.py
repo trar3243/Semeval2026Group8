a1cf45af96c511f4fca75c4708ef678006f5ee83
Troy <trar3243@colorado.edu>
2025-12-06 08:44:41 -0700

    write_out_devset_save_models
    Added modes for evaluation and training. Had writeout of epoch 4,
    however this saved model is 4Gb, too large for git. Trying to find way
    around this issue.

--- ClassDefinition/Dataset.py
+++ ClassDefinition/Dataset.py
@@ -63,25 +63,38 @@ class Dataset:
     def __init__(
         self, 
         dataPath, 
-        robertaA, robertaB, robertaD, robertaG, robertaH
+        robertaA, robertaB, robertaD, robertaG, robertaH,
+        eval_mode, devSetPath, trainSetPath 
     ):
-        self.__set_entry_list__(dataPath)
-        self.__set_user_indices__()
+        self.dataPath = dataPath
+        self.devSetPath=devSetPath
+        self.trainSetPath=trainSetPath
         self.robertaA = robertaA
         self.robertaB = robertaB
         self.robertaD = robertaD
         self.robertaG = robertaG
         self.robertaH = robertaH
-        self.trainSet, self.devSet = train_test_split(self.entryList, test_size=0.1, random_state=42)
+        
+        if(eval_mode == False):
+            self.entryList = self.__get_entry_list__(self.dataPath)
+            self.trainSet, self.devSet = train_test_split(self.entryList, test_size=0.1, random_state=42)
+        else:
+            self.devSet = self.__get_entry_list__(self.devSetPath) 
+            self.trainSet = self.__get_entry_list__(self.trainSetPath) 
+            self.entryList = self.devSet + self.trainSet
+        
+        self.__set_user_indices__()
         self.trainBatchList = None
         self.devBatchList = None
 
-    def __set_entry_list__(self, dataPath):
-        self.entryList = []
+    def __get_entry_list__(self, dataPath):
+        entryList = []
         with open(dataPath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                self.entryList.append(Entry(row))
+                entryList.append(Entry(row))
+        # entryList = entryList[:20]
+        return entryList
 
     def __set_user_indices__(self):
         user_id_list = [e.user_id for e in self.entryList]
@@ -109,3 +122,37 @@ class Dataset:
         valence_class_counts = Counter(item.valence_class for item in self.entryList)
         arousal_class_counts = Counter(item.arousal_class for item in self.entryList)
         print(f"EntryList has {len(self.entryList)} entries, {self.number_of_users} distinct users with following label distribution:\n\tvalence:{valence_class_counts}\n\tarousal:{arousal_class_counts}")
+    def __convert_float_to_bool__(self, is_words):
+        if(is_words == 1.0):
+            return "True"
+        elif(is_words == 0.0):
+            return "False"
+        raise Exception(f"{is_words} not in [0.0,1.0]")
+    def writeOutSet(self, entrySet, path):
+        rows = []
+        for entry in entrySet:
+            rows.append({
+                "user_id":entry.user_id, 
+                "text_id":entry.text_id,
+                "text":entry.text, 
+                "timestamp":entry.timestamp, 
+                "collection_phase":entry.collection_phase, 
+                "is_words":self.__convert_float_to_bool__(entry.is_words),
+                "valence":entry.valence,
+                "arousal":entry.arousal
+            })
+        fieldnames = ["user_id", "text_id", "text", "timestamp", "collection_phase", "is_words", "valence", "arousal"]
+
+        with open(path, "w", encoding="utf-8", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
+            writer.writeheader()
+            writer.writerows(rows)
+            f.close()
+
+    def writeOutDevSet(self):
+        self.writeOutSet(self.devSet, self.devSetPath)
+        print(f"Completed write out of dev set to {self.devSetPath}")
+    def writeOutTrainSet(self):
+        self.writeOutSet(self.trainSet, self.trainSetPath)
+        print(f"Completed write out of train set to {self.trainSetPath}")
+        
